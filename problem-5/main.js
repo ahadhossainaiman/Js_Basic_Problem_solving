b0cd5d49e5de7254eb6e5723f60cdb83e94f2128
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2021-08-17 13:08:20 -0700

    problem-5

---
+++ problem-5/main.js
@@ -0,0 +1,13 @@
+let animateString = (id) =>{
+    let element = document.getElementById(id)
+    console.log(element);
+    let textNode = element.childNodes[0];
+    console.log(textNode)
+    let text = textNode.data;
+    console.log(text[text.length-1])
+
+    setInterval(()=>{
+        text = text[text.length - 1] + text.substring(0,text.length-1);
+        textNode.data = text;
+    },1000);
+}
